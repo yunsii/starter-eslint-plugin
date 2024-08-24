7d5ca93fe2f5f17e7ff4a4ceca97bb726e815d70
Yuns <yuns.xie@qq.com>
2024-08-24 17:54:45 +0800

    ♻️ chore: update test

--- src/rules/hello.test.ts
+++ src/rules/hello.test.ts
@@ -1,12 +1,12 @@
 import { $, createRuleTester } from 'eslint-vitest-rule-tester'
 import { describe, it } from 'vitest'
 
-import { rule as hello } from './hello'
+import { rule } from './hello'
 
 describe('hello', () => {
   const { valid } = createRuleTester({
     name: 'hello',
-    rule: hello,
+    rule,
   })
 
   it('valid case 1', () => {
